4b60c3265a723074e4c5d71dfe26d8e03f90d7b8
Caryourday <adetkay@gmail.com>
2018-02-06 18:04:38 -0500

    Working on LAB #

--- app/src/main/java/rbs/caryourday/androidlabs/LoginActivity.java
+++ app/src/main/java/rbs/caryourday/androidlabs/LoginActivity.java
@@ -1,13 +1,74 @@
 package rbs.caryourday.androidlabs;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+
 
 public class LoginActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME="LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /*Making a reference to the button in the .xml file */
+        Button loginbutton = findViewById(R.id.button2);
+
+        /*Giving the button some function*/
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+
+            }
+        });
+
+
+        /*Saving the user's input*/
+        /*Accessing the strings.xml string*/
+       SharedPreferences s = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+    }
+
+    protected void launchActivity(){
+        /*Switching to the other Activity*/
+        Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+
+        /*This is what will take us to StartActivity*/
+        startActivity(intent);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
     }
 }
--- app/src/main/java/rbs/caryourday/androidlabs/StartActivity.java
+++ app/src/main/java/rbs/caryourday/androidlabs/StartActivity.java
@@ -1,13 +1,66 @@
 package rbs.caryourday.androidlabs;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class StartActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME="StartActivity";
+    Button  startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        /*Storing reference to StartActivity button*/
+        startButton = findViewById(R.id.button);
+
+        /*Giving the button some function*/
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+                /*Method call to switch to the ListItems Activity*/
+                launchListItemsActivity();
+            }
+        });
+    }
+    /* The method for the onclickview view - launchListItemsActivity*/
+    protected void launchListItemsActivity() {
+        Intent intent = new Intent(StartActivity.this, ListItemsActivity.class);
+
+        startActivityForResult(intent, 50);
+    }
+
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+    }
+
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
     }
 }
